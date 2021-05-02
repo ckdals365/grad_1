c6194f6af39ab579855cc127eb44a4ddd06e673e
limchangmin <dlackdddals@gmail.com>
2021-05-02 22:28:32 +0900

    simple web server running at localhost:8000

--- MyHandler.js
+++ MyHandler.js
@@ -1,12 +1,12 @@
 function start(res) {
-    let body = '가습기 정보 : <br> 가습기 상태 : <br> 가습기 설정 치 : ';
+    let body = 'hum portnumber';
     res.writeHead(200, {'content-Type': 'text/html'});
     res.write(body);
     res.end();
 }
 
 function hello(res) {
-    let body = 'This is my first web server.';
+    let body = 'hum information : <br> hum state : <br> hum set : ';
     res.writeHead(200, {'content-Type': 'text/html'});
     res.write(body);
     res.end();
