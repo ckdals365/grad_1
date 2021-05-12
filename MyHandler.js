9f5cd83bff992f6364578989914ece69b0ff9100
limchangmin <dlackdddals@gmail.com>
2021-05-12 11:12:12 +0900

    add random wait page

--- MyHandler.js
+++ MyHandler.js
@@ -3,6 +3,7 @@ function start(res) {
     body += '<body><div>Hello, world! <br> I am in the cloud class.</div><br>';
     body += '<div><a href="/hello">hello 페이지</a></div>'
     body += '<div><a href="/wait">5초 대기 페이지</a></div>'
+    body += '<div><a href="/randomWait">무작위 대기 페이지</a></div>'
     body += '</body>'
     res.writeHead(200, { 'content-Type': 'text/html', 'charset': 'UTF-8' });
     res.write(body);
@@ -25,6 +26,17 @@ function wait(res) {
     }, 5000);
 }
 
+function randomwait(res) {
+    let waitTime = Math.round(Math.random()*10000.);
+    setTimeout(function () {
+        let body = 'Thank you for waiting ' + waitTime + ' ms.';
+        res.writeHead(200, { 'content-Type': 'text/html' });
+        res.write(body);
+        res.end();
+    }, waitTime);
+}
+
 exports.start = start;
 exports.hello = hello;
 exports.wait = wait;
+exports.randomWait = randomWait;
