623c0c415905cf08dec23b0585777e1dd9b8209c
limchangmin <dlackdddals@gmail.com>
2021-05-12 12:09:27 +0900

    add server info page

--- MyHandler.js
+++ MyHandler.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const os = require('os');
 
 function start(res) {
     let body = '<head><meta charset = "UTF-8"/></head>'
@@ -7,6 +8,8 @@ function start(res) {
     body += '<div><a href="/wait">5초 대기 페이지</a></div>'
     body += '<div><a href="/randomWait">무작위 대기 페이지</a></div>'
     body += '<div><a href="/firstHtml">HTML 읽는 페이지</a></div>'
+    body += '<div><a href="/page">Handler 없이 "/page"로 매핑하는 페이지</a></div>'
+    body += '<div><a href="/serverInfo">Server 정보를 표시하는 페이지</a></div>'
     body += '</body>'
     res.writeHead(200, { 'content-Type': 'text/html', 'charset': 'UTF-8' });
     res.write(body);
@@ -47,7 +50,14 @@ function htmlFile(res, file) {
 }
 
 function firstHtml(res) {
-htmlFile(res, './firstHtml.html');
+    htmlFile(res, './firstHtml.html');
+}
+
+function serverInfo(res) {
+    info = JSON.stringify(os.cpus());
+    res.writeHead(200, { 'content-Type': 'text/html' });
+    res.write(info);
+    res.end();
 }
 
 exports.start = start;
@@ -56,3 +66,4 @@ exports.wait = wait;
 exports.randomWait = randomWait;
 exports.firstHtml = firstHtml;
 exports.htmlFile = htmlFile;
+exports.serverInfo = serverInfo;
